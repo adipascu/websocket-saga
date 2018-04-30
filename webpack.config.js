d413cf6d6b197028daba564a594b97e4f953d309
Adi Pascu <mail@adipascu.ro>
2018-04-30 13:36:38 +0300

    Do not bundle redux-saga

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
-const webpack = require('webpack');
 const path = require('path');
 
-module.exports = () => {
-  return {
-    target: 'electron-renderer',
-    mode: 'production',
-    // mode: 'development',
-    // devtool: 'none',
-    node: {
-      __dirname: false,
-      __filename: false,
+module.exports = () => ({
+  target: 'electron-renderer',
+  // do not bundle redux-saga
+  externals: {
+    'redux-saga': {
+      commonjs2: 'redux-saga',
     },
-    entry: './src/main.js',
-    // entry: './src/second.js',
-    output: {
-      path: path.join(__dirname, 'lib'),
-      libraryTarget: "commonjs2",
+    'redux-saga/effects': {
+      commonjs2: 'redux-saga/effects',
     },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: path.resolve(__dirname, 'src'),
-          use: [
-            {
-              loader: 'babel-loader',
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js'],
-    },
-  };
-};
+  },
+  mode: 'production',
+  // mode: 'development',
+  // devtool: 'none',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, 'lib'),
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+});
